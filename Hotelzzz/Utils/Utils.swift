39fa11d3c0966a1b3c2b4bc1a35bf1a8a1549d22
willysharp10 <willysharp10@yahoo.com>
2018-04-06 00:14:12 -0700

    Clean up Util code to return address without \n character

--- Hotelzzz/Utils/Utils.swift
+++ Hotelzzz/Utils/Utils.swift
@@ -10,16 +10,19 @@ import UIKit
 
 struct Utils {
     static func cleanUpData(dic: NSDictionary) -> hotelInfo {
+        //flatten data to an array and split by semi column
         let stringData = (dic.flatMap({ (key, value) -> String in
             return "\(value)"
         }) as Array).joined(separator: ";")
         
+        //split data by equal sign into an array
         let anotherComponent = stringData.components(separatedBy: "=") as Array
         /*print("address: \(anotherComponent[2])") //address
          print("imageURL: \(anotherComponent[4])") //imageURL
          print("name: \(anotherComponent[5])") //name
          print("price: \(anotherComponent[6])") //price*/
         
+        //extract the data from teh array location and split by semi column again
         let addressToSplit = anotherComponent[2]
         let address = addressToSplit.components(separatedBy: ";")
         
@@ -38,10 +41,11 @@ struct Utils {
          print(name[0])
          print(price[0])*/
         
+        
         let hotelData = hotelInfo()
-        hotelData.address = address[0]
-        hotelData.imageURL = imageURL[0]
-        hotelData.name = name[0]
+        hotelData.address = (address[0].replacingOccurrences(of: "\"", with: "")).replacingOccurrences(of: "\\n", with: " ") //remove double quotes
+        hotelData.imageURL = imageURL[0].replacingOccurrences(of: "\"", with: "") 
+        hotelData.name = name[0].replacingOccurrences(of: "\"", with: "")
         hotelData.price = price[0]
         
         return hotelData
